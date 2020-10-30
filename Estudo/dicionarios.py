d375bd476df0428d440b27a378504eb8cb8055c6
Vitor Gomes <francisco.fvg001@gmail.com>
2020-10-30 14:49:51 -0300

    Estudo

--- Estudo/dicionarios.py
+++
@@ -1,8 +0,0 @@
-dicionario = {}
-
-#Incremento de itens em um dicionário através de input
-for i in range(2):
-    en1 = input('nova coluna')
-    dicionario[en1] = input().split()
-
-print(dicionario)
